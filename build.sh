6581c7f99b909339edfeb384c0e51e4021b6fc24
susheelb <susheels88@gmail.com>
2025-03-31 21:33:57 +0530

    added build flags

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
 clang -O2 -g -target bpf  -D__TARGET_ARCH_x86 -o ./socket.bpf.o -c ./socket.bpf.c
 bpftool  gen skeleton socket.bpf.o name socket > socket.skel.h
-cc -lbpf -o socket  `pkg-config --cflags glib-2.0` ./socket.c  `pkg-config --libs glib-2.0` ./libbpf.a
+cc -lbpf -lelf -lz -ldl -o socket  `pkg-config --cflags glib-2.0` ./socket.c  `pkg-config --libs glib-2.0` ./libbpf.a
